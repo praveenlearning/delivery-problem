28fade12c8d673fceb74cf6df9860bcbce8bb813
praveen <praveen.bathula@thoughtworks.com>
2022-07-23 10:04:59 +0530

    Add tests for deliver packages

--- src/test/java/VehicleTest.java
+++ src/test/java/VehicleTest.java
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -38,5 +40,51 @@ class VehicleTest {
         var timeToDeliver = vehicle.calculateTime(pkg);
 
         assertEquals(2, timeToDeliver);
+    }
+
+    @Test
+    public void shouldReturnDeliveryReportForPackages() {
+        List<Package> packages = new ArrayList<>() {{
+            add(Package.parsePackage("P1 50 30"));
+            add(Package.parsePackage("P2 120 80"));
+            add(Package.parsePackage("P3 90 150"));
+        }};
+        Vehicle vehicle = new Vehicle(60, 300);
+
+        var expected = new HashMap<>() {{
+            put(packages.get(0), 0.5);
+            put(packages.get(1), 1.33);
+            put(packages.get(2), 2.5);
+        }};
+        Map<Package, Double> deliveryReport = vehicle.deliver(packages);
+
+        assertEquals(expected, deliveryReport);
+    }
+
+    @Test
+    public void shouldReturnDeliveryReportForPackagesGivenItHadPreviousDelivery() {
+        List<Package> packages = new ArrayList<>() {{
+            add(Package.parsePackage("P1 50 30"));
+            add(Package.parsePackage("P2 120 80"));
+            add(Package.parsePackage("P3 90 150"));
+        }};
+
+        List<Package> otherPackages = new ArrayList<>() {{
+            add(Package.parsePackage("P1 45 75"));
+            add(Package.parsePackage("P2 100 20"));
+            add(Package.parsePackage("P3 60 180"));
+        }};
+
+        Vehicle vehicle = new Vehicle(60, 300);
+
+        var expected = new HashMap<>() {{
+            put(otherPackages.get(0), 6.25);
+            put(otherPackages.get(1), 5.33);
+            put(otherPackages.get(2), 8.0);
+        }};
+        vehicle.deliver(packages);
+        Map<Package, Double> deliveryReport = vehicle.deliver(otherPackages);
+
+        assertEquals(expected, deliveryReport);
     }
 }
